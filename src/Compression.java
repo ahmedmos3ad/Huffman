84b74e60aeacf63475be62d2a18cbfa5121bf041
dominatorz <ahmed.mosad555@gmail.com>
2019-12-22 03:09:59 +0200

    COMPRESSION FIX

--- src/Compression.java
+++ src/Compression.java
@@ -1,18 +1,25 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map.Entry;
 
 public class Compression {
-	
+	StringBuilder builder=new StringBuilder();
+	char cd;
+	int pointer2=0;
 	public void Compress(String filepath) throws IOException 
 	{
-	    BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
+		BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
 	    int pointer=0;
 	    char la;
 	    String write;
 	    la = Character.toLowerCase(Frequency.sb.charAt(pointer++));
-	    
 	    for (Entry<Character, String> entry : HuffmanTree.code.entrySet())
 		{
 	    	writer.write(entry.getKey());
@@ -26,11 +33,49 @@ public class Compression {
 	    {
 	    	//check for space as we replaced it with capital S in the hash map
 	    	if (Character.isWhitespace(la))
-				la='S';
+			la='S';
 	    	write=HuffmanTree.code.get(la);
 	    	writer.write(write);
+	    	builder.append(write);
 	    	la = Character.toLowerCase(Frequency.sb.charAt(pointer++));
 	    }
+	    builder.append('$');
 	    writer.close();
+	    bitStream(filepath);
+	}
+	
+	public void bitStream(String filepath) throws IOException
+	{
+	BufferedWriter writer = new BufferedWriter(new FileWriter("Testing.txt"));
+	int currentByte=0;
+	int numOfBitsInCurrentByte=0;
+	int byte1;
+	cd=builder.charAt(pointer2++);
+	File file=new File("Testing.txt");
+	OutputStream output = new BufferedOutputStream(new FileOutputStream(file));
+	for (Entry<Character, String> entry : HuffmanTree.code.entrySet())
+	{
+    	writer.write(entry.getKey());
+    	writer.write('=');
+    	writer.write(entry.getValue());
+    	writer.write("\n");
+	}
+	writer.write("$$\n");
+	while (cd!='$')
+	{
+		if (cd=='1')
+			byte1=1;
+		else byte1=0;
+		currentByte = currentByte >> 1|byte1;
+		//System.out.println(currentByte);
+		numOfBitsInCurrentByte++;
+		if (numOfBitsInCurrentByte==8) {
+			output.write(currentByte);
+		    numOfBitsInCurrentByte = 0;
+		}
+		cd=builder.charAt(pointer2++);
+	}
+	output.close();
+	writer.close();
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,47 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Scanner;
 
 public class Main {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException {
-		Frequency frequency=new Frequency();
-		frequency.getFreqencies("test.txt");
-		HuffmanTree tree=new HuffmanTree();
-		Compression compress=new Compression();
-		compress.Compress("test.txt");
-		
+
+		String filepath;
+		char ch;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Please Enter the file you Want to Compress/Decompress");
+		do    
+	       {
+	           System.out.println("\nHuffman Compression and Decompression\n");
+	           System.out.println("1. Compress ");
+	           System.out.println("2. Decompress");
+	           
+	           int choice = scan.nextInt();            
+	           switch (choice)
+	           {
+	           case 1 : 
+	        	   System.out.println("Please Enter the file you Want to Compress\n");
+	        	   filepath=scan.next();
+	        	   Frequency frequency=new Frequency();
+	       		   frequency.getFreqencies("test.txt");
+	       		   HuffmanTree tree=new HuffmanTree();
+	       		   Compression compress=new Compression();
+	       		   compress.Compress(filepath);
+	               break;                          
+	           case 2 : 
+	       		   //decompress.decompress(filepath);
+	               break;                                          
+	           default : 
+	               System.out.println("Wrong Entry \n ");
+	               break;    
+	           }
+	           System.out.println("\nDo you want to continue (Type y or n) \n");
+	           ch = scan.next().charAt(0);                        
+	       } while (ch == 'Y'|| ch == 'y');               
+	    }
 	
 	
 	
 	}
-}
+
 
